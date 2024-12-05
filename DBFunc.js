10f406a520e961b8ebf6bb936c6b4308cc948180
acontre42 <152231739+acontre42@users.noreply.github.com>
2024-12-05 15:55:42 -0800

    Changed code to ES6 module

--- DBFunc.js
+++ DBFunc.js
@@ -1,11 +1,33 @@
+/*
 const path = require('path');
 require('dotenv').config({
     override: true,
     path: path.join(__dirname, 'development.env')
 });
+*/
+import path from 'path'; // *** 
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
+dotenv.config({
+    override: true,
+    path: path.join(__dirname, 'development.env')
+});
 
-const {Client, Pool} = require('pg');
+//const {Client, Pool} = require('pg');
+/*
 const pool = new Pool({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT
+});
+*/
+import pg from 'pg'; // ***
+const pool = new pg.Pool({
     user: process.env.USER,
     host: process.env.HOST,
     database: process.env.DATABASE,
@@ -23,7 +45,7 @@ export async function getNumMovies() {
             throw new Error('Empty database');
         }
         else {
-            console.log(numMovies);
+            //console.log(numMovies);
             return numMovies;
         }
     }
@@ -44,7 +66,7 @@ export async function getRandomMovie(max) {
     const client = await pool.connect();
     try {
         let randomId = Math.floor(Math.random() * max) + 1;
-        queryString = 
+        let queryString = 
         `SELECT title, year, wiki_link, im_link 
         FROM movie 
         JOIN wikipedia_link
@@ -53,7 +75,7 @@ export async function getRandomMovie(max) {
         ON movie.id = imdb_link.movie_id
         WHERE id = ${randomId}`;
         let {rows} = await client.query(queryString);
-        console.log(randomId, rows[0]['title'], rows[0]['year'], rows[0]['wiki_link'], rows[0]['im_link']);
+        //console.log(randomId, rows[0]['title'], rows[0]['year'], rows[0]['wiki_link'], rows[0]['im_link']);
 
         return {
             index: randomId,
@@ -65,6 +87,62 @@ export async function getRandomMovie(max) {
     }
     catch(err) {
         console.log(err);
+        return null;
+    }
+    finally {
+        client.release();
+    }
+}
+
+export async function getAllMovies() {
+    const client = await pool.connect();
+    try {
+        let queryString = 'SELECT * FROM movie';
+        let {rows} = await pool.query(queryString);
+        let allMovies = [];
+        rows.forEach((row) => {
+            allMovies.push({
+                id: row['id'],
+                title: row['title'],
+                year: row['year']
+            })
+        });
+        return allMovies;
+    }
+    catch (err) {
+        console.log(err);
+        return [];
+    }
+    finally {
+        client.release();
+    }
+}
+
+export async function getMovieById(id) {
+    if (!id || isNaN(id)) {
+        console.log('ERROR: id is not a valid number');
+        return null;
+    }
+
+    const client = await pool.connect();
+    try {
+        const queryString = `SELECT * FROM movie WHERE id = ${id}`;
+        let {rows} = await pool.query(queryString);
+        if (!rows[0]) {
+            console.log(`ERROR: no movie with id ${id}`);
+            return null;
+        }
+
+        let movie = {
+            id: rows[0]['id'],
+            title: rows[0]['title'],
+            year: rows[0]['year']
+        };
+        return movie;
+    }
+    catch(err) {
+        console.log(err);
+        return null;
     }
     finally {
         client.release();
